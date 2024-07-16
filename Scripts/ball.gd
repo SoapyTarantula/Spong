3bfedc89995673e109d1413d626429ca9f6c963c
Soapy <soapytarantula@gmail.com>
2024-07-16 09:47:45 -0500

    Add ball collision safety
    Added a check to make sure the ball can't collide more than once since the last frame.

--- Scripts/ball.gd
+++ Scripts/ball.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 @export var speed: int = 400
 @onready var hit_sound = $hitSound
+var can_collide = true
 
 func _ready():
 	self.position = get_viewport_rect().size / 2
@@ -9,13 +10,16 @@ func _ready():
 	velocity = Vector2(randi_range(-350, 350), speed)
 	hit_sound.volume_db = linear_to_db(Music.game_vol)
 
-func _physics_process(_delta):
+func _physics_process(delta):
 	var collision_info = get_last_slide_collision()
-	if collision_info:
+	if collision_info and can_collide:
+		can_collide = false
 		var normal = collision_info.get_normal()
 		hit_sound.set_pitch_scale(randf_range(0.75, 1.25))
 		hit_sound.play()
 		velocity = velocity.bounce(normal)
+		await get_tree().create_timer(delta).timeout
+		can_collide = true
 	
 	velocity = velocity.normalized() * speed
 	move_and_slide()
